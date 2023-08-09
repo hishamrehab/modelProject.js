16d8ac29f5f047c656666a40880de1d0291d6425
hisham rehab <128677920+hishamrehab@users.noreply.github.com>
2023-08-09 23:17:06 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const modelBtn = document.querySelector(".modal-btn");
 const bannerSection = document.querySelector(".banner");
 const modelOverlay= document.querySelector(".model-overlay");
 const closeBtn = document.querySelector(".close-btn");
+const Body = document.querySelector("body");
+
+
+
+
 
 modelBtn.addEventListener("click", function () {
     bannerSection.style.display = "none";
-     modelOverlay.style.display = "block";
+  modelOverlay.style.display = "block";
+    Body.style.opacity = ".5";
+
 });
 
 closeBtn.addEventListener("click", function () {
     modelOverlay.style.display = "none";
-      bannerSection.style.display = "block";
+  bannerSection.style.display = "block";
+     Body.style.opacity = "1";
 })
 
 
